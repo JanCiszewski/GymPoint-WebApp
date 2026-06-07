2617660c30ddedef3ef8390973fbee619fd8693f
Jan Ciszewski <janciszewski36@gmail.com>
2026-06-07 19:30:12 +0200

    Done connection with database

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv")
+
+const database = require("./config/db")
 
 const app = express();
+dotenv.config();
+
+database();
 
 app.listen(3000, () => {
     console.log("Serwer działa na porcie 3000");
